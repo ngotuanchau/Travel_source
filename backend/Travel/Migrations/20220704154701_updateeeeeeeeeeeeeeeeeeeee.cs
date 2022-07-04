1888ef20e58a079f7f124eb1ba8c70b83c489a07
chaungo.100801@gmail.com <chaungo.100801@gmail.com>
2022-07-04 22:47:56 +0700

    commit api dat tour

---
+++ backend/Travel/Migrations/20220704154701_updateeeeeeeeeeeeeeeeeeeee.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Travel.Migrations
+{
+    public partial class updateeeeeeeeeeeeeeeeeeeee : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "ThoiGianId",
+                table: "HoaDons",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ThoiGianId",
+                table: "HoaDons");
+        }
+    }
+}
