e469a0db4e428f58f92dcf35fceb7819167a6dd6
B.Tuyen <79151413+Tuyen1612@users.noreply.github.com>
2022-07-10 20:42:52 +0700

    commit profile

---
+++ frontend/tourshop_web/src/app/user/pages/profile/profile.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from "@angular/core";
+
+import { NguoiDungsService } from "../../../service/nguoidungs.service";
+import { DatePipe } from "@angular/common";
+@Component({
+  selector: "app-profile",
+  templateUrl: "./profile.component.html",
+  styleUrls: ["./profile.component.scss"],
+})
+export class ProfileComponent implements OnInit {
+  idUser: any;
+  user: any;
+  constructor(private userService: NguoiDungsService) {}
+
+  ngOnInit(): void {
+    this.idUser = localStorage.getItem("id");
+    this.getUser();
+  }
+  getUser() {
+    this.userService.getUser(this.idUser).subscribe((res) => {
+      if (res != null) {
+        this.user = res;
+      }
+      console.log(this.user);
+    });
+  }
+  updateProfile() {}
+}
