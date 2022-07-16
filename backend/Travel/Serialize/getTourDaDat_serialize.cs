d5acef2d6b000b6e1e194695db22ebc72d948585
chaungo.100801@gmail.com <chaungo.100801@gmail.com>
2022-07-16 20:41:40 +0700

    get tour da dat

---
+++ backend/Travel/Serialize/getTourDaDat_serialize.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Travel.Serialize
+{
+    public class getTourDaDat_serialize
+    {
+        public int id { get; set; }
+        public int nguoidungid { get; set; }
+        public string tentour { get; set; }
+        public string tencongty { get; set; }
+        public int thoigianid { get; set; }
+        public DateTime ngaykh { get; set; }
+        public string diemdi { get; set; }
+        public int sovenguoilon { get; set; }
+        public int sovetrenho { get; set; }
+        public int sovetreem { get; set; }
+        public decimal tongtien { get; set; }
+        public int trangthaitour { get; set; }
+        public int trangthaihoadon { get; set; }
+        public string ngaydat { get; set; }
+    }
+}
