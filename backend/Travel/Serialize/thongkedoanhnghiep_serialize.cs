bacb465a34c9ee2d71ad2ed25429e9775b810400
chaungo.100801@gmail.com <chaungo.100801@gmail.com>
2022-07-17 02:18:52 +0700

    commit code thong ke

---
+++ backend/Travel/Serialize/thongkedoanhnghiep_serialize.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Travel.Serialize
+{
+    public class thongkedoanhnghiep_serialize
+    {
+        public int iddoanhnghiep { get; set; }
+        public string thang { get; set; }
+        public int tongsotour { get; set; }
+        public int sotour_dahuy { get; set; }
+        public int sotour_chuanbi { get; set; }
+        public int sotour_batdau { get; set; }
+        public int sotour_hoanthanh { get; set; }
+        public decimal tongdoanhthu { get; set; }
+    }
+}
