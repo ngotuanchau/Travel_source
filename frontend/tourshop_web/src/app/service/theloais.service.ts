f30bfd0b098ffe8b532c99e36f6ae085bf24aba7
B.Tuyen <79151413+Tuyen1612@users.noreply.github.com>
2022-07-21 04:18:34 +0700

    CRUD TheLoai

--- frontend/tourshop_web/src/app/service/theloais.service.ts
+++ frontend/tourshop_web/src/app/service/theloais.service.ts
@@ -1,16 +1,37 @@
-import { HttpClient } from '@angular/common/http';
-import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { Observable } from "rxjs";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class TheloaisService {
-  baseUrl='http://localhost:40998/api/TheLoai';
-  constructor(private http:HttpClient) { }
+  baseUrl = "http://localhost:40998/api/TheLoai";
+  constructor(private http: HttpClient) {}
 
   //Get all The loai
-  getTheLoai():Observable<any>{
-    return this.http.get<any>(this.baseUrl+'/get_theloai');
+  getTheLoai(): Observable<any> {
+    return this.http.get<any>(this.baseUrl + "/get_theloai");
+  }
+  //Create
+  create(theloai: any): Observable<any> {
+    return this.http.post<any>(this.baseUrl + "/admin/create_theloai", theloai);
+  }
+  //Update
+  update(id: any, theloai: any): Observable<any> {
+    return this.http.post<any>(
+      this.baseUrl + "/admin/update_theloai/" + id,
+      theloai
+    );
+  }
+  //delete
+  delete(id: any) {
+    return this.http.delete<any>(this.baseUrl + "/admin/delete_theloai/" + id);
+  }
+  //khôi phục
+  khoiPhuc(id: any) {
+    return this.http.delete<any>(
+      this.baseUrl + "/admin/khoiphuc_theloai/" + id
+    );
   }
 }
